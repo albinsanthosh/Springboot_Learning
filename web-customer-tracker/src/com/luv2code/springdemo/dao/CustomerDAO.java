1d111a2a24c7573a9fb946c768f08f12abd9e1b5
VF-ROOT\santa003 <albin.santhosh@vcontractor.co.za>
2023-01-31 23:06:05 +0530

    webapp spring mvc update, delete, search, sort customer

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAO.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAO.java
@@ -7,4 +7,14 @@ import com.luv2code.springdemo.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+
+	public List<Customer> getCustomers(int theSortField);
 }
--- web-customer-tracker/src/com/luv2code/springdemo/service/CustomerService.java
+++ web-customer-tracker/src/com/luv2code/springdemo/service/CustomerService.java
@@ -7,5 +7,15 @@ import com.luv2code.springdemo.entity.Customer;
 public interface CustomerService {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+
+	public List<Customer> getCustomers(int theSortField);
 	
 }
