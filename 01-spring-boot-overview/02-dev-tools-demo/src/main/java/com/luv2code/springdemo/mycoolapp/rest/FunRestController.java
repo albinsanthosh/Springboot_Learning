7beebb0778132eac350e6e59a19373d197ce5888
albin <albin.santhosh98@gmail.com>
2023-04-11 21:59:23 +0530

    springboot overview

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springdemo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // expose new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expose new endpoint for "workout"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+}
